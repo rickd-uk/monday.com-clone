26c38608a35f6fe991f1d313ae699c6007e0eb3b
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2022-04-22 12:10:43 +0900

    add: react router & set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+
 const App = () => {
-	return <div className='App'></div>
+	return (
+		<div className='App'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={Dashboard}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
